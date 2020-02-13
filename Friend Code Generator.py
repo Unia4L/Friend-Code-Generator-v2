2e6b91c1fb60f33eb8c0fea4f979a283bc81f165
Warwick4L <58864511+Warwick4L@users.noreply.github.com>
2020-02-13 18:21:22 +0000

    Update Friend Code Generator.py

--- Friend Code Generator.py
+++ Friend Code Generator.py
@@ -4,18 +4,18 @@ import time
 import os
 from random import randint
 clear = lambda: os.system('cls')
-ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.1")
+ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.2")
 
 def menu():
-    print("Last Update 4th January 2020")
+    choicemenu = 0
+    print("Last Update - 2nd February 2020")
     print("----------------------")
-    print(" 1 - 3ds/WiiU Friend Code Generator")
+    print(" 1 - 3ds/Wii-U Friend Code Generator")
     print(" 2 - Switch Friend Code Generator")
     print(" 3 - Clear Codes.txt")
-    print(" 4 - Settings")
     print("-----------------------")
     print("")
-    print("Type 1, 2, 3 or 4 then press ENTER")
+    print("Type 1, 2 or 3 then press ENTER")
     choicemenu = input("")
     if choicemenu == "1":
         clear()
@@ -27,15 +27,16 @@ def menu():
         time.sleep(0.2)
         os.remove("Codes.txt")
         file = open("Codes.txt","w") 
-        print("Done! Closing in 1 second...")
+        print("Deleted!")
+        file.close()
         time.sleep(1)
-        exit()
-    if choicemenu == "4":
         clear()
-        input("Feature coming soon")
+        menu()
     else:
     	print("Wrong input. Choose 1 to 4.")
-    	input()
+    	time.sleep(0.5)
+    	clear()
+    	menu()
 
 
 def wiiu():
@@ -45,22 +46,19 @@ def wiiu():
     clear()
     print("3ds and Wii-U Friend Code Generator")
     print("")
-    print("How many codes do you want to generate")
+    print("How many codes to generate: ")
     amt = int(input())
-    print("Press enter to generate " + str(amt) + " 3ds/Wii-U friend codes.")
+    print("Generating...")
     total = 0
     file = open("codes.txt","w") 
     while total < amt:
-        fcpt1 = randint(1000, 9999)
-        fcpt2 = randint(1000, 9999)
-        fcpt3 = randint(1000, 9999)
+        fcpt1 = str(randint(1000, 9999)) + "-" + str(randint(1000, 9999)) + "-" + str(randint(1000,9999))
         total = total+1
-        code = str(fcpt1)+"-"+str(fcpt2)+"-"+str(fcpt3)
-        file.write(code+"\n")
-        ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.0 "+ str(total) +" generated")
-    clear
+        file.write(str(fcpt1)+"\n")
+    clear()
+    file.close()
     print("Completed! Generated "+ str(total) +" codes!")
-    ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.0")
+    ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.2")
     a = input("Press enter to close! Thanks for using!")
     close()
 
@@ -71,22 +69,19 @@ def switch():
     clear()
     print("Switch Friend Code Generator")
     print("")
-    print("How many codes do you want to generate")
+    print("How many codes to generate: ")
     amt = int(input())
-    print("Press enter to generate " + str(amt) + " 3ds/Wii-U friend codes.")
+    print("Generating...")
     total = 0
-    file = open("codes.txt","w") 
+    file = open("Codes.txt","w") 
     while total < amt:
-        fcpt1 = randint(1000, 9999)
-        fcpt2 = randint(1000, 9999)
-        fcpt3 = randint(1000, 9999)
+        fcpt1 = str(randint(1000, 9999)) + "-" + str(randint(1000, 9999)) + "-" + str(randint(1000,9999))
         total = total+1
-        code = "SW-"+str(fcpt1)+"-"+str(fcpt2)+"-"+str(fcpt3)
-        file.write(code+"\n")
-        ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.0 "+ str(total) +" generated")
+        file.write("SW-" + str(fcpt1)+"\n")
     clear()
+    file.close()
     print("Completed! Generated "+ str(total) +" codes!")
-    ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.0")
+    ctypes.windll.kernel32.SetConsoleTitleW("Friend Code Generator - 2.0.2")
     a = input("Press enter to close! Thanks for using!")
     close()
         
