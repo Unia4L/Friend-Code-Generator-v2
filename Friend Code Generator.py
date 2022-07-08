81a2ab0e8a9d63d6cfd2fc569b21350aa9d52bea
Unia <58864511+Unia4L@users.noreply.github.com>
2022-07-08 05:03:56 +0100

    Add files via upload

--- Friend Code Generator.py
+++ Friend Code Generator.py
@@ -1,133 +1,66 @@
-import math
 import ctypes
-import time
 import os
 from random import randint
+
 clear = lambda: os.system('cls')
 title = ctypes.windll.kernel32.SetConsoleTitleW
-name = "Friend code generator"
 ver = "2.0.5"
 
+def generator(amount, type):
+    file = open("codes.txt","w")
+    total = 0
+    for i in range(total, amount):
+        total += 1
+        match type:
+            case 0:
+                friendcode = f"{str(randint(1000,9999))}-{str(randint(1000,9999))}-{str(randint(1000,9999))}"
+                title(f"Codes generated: {total}")
+            case 1:
+                friendcode = f"SW-{str(randint(1000,9999))}-{str(randint(1000,9999))}-{str(randint(1000,9999))}"
+                title(f"Codes generated: {total}")
+            case 2:
+                friendcode = str(randint(1,999999999))
+                title(f"Codes generated: {total}")
+        file.write(f"{friendcode}\n")
+    file.close()
+    input("Generation finished! Press enter to close")
+
+
 def menu():
-    print("")
-    print("----------------------")
+    clear()
+    title(f"Friend Code Generator {ver}")
+    while True:
+        try:
+            amount = int(input("Codes to generate: "))
+            break
+        except Exception:
+            print("Put a valid number.")
+    clear()
+    print("What type of code do you want to generate?")
     print(" 1 - 3ds/Wii-U friend code generator")
     print(" 2 - Switch friend code generator")
     print(" 3 - Steam friend code generator")
-    print(" 4 - Clear Codes.txt")
-    print("-----------------------")
     print("")
-    print("Type 1, 2, 3 or 4 then press ENTER")
-    title("[" + str(ver) + "] " + str(name))
-    x = input("")
-    if x == "1":
-        clear()
-        wiiu()
-    if x == "2":
-        clear()
-        switch()
-    if x == "3":
-        clear()
-        steam()
-    if x == "4":
-        time.sleep(0.2)
-        os.remove("Codes.txt")
-        file = open("Codes.txt","w") 
-        print("Deleted!")
-        file.close()
-        time.sleep(1)
-        clear()
-        menu()
-    else:
-        print("Wrong input. Choose 1 to 4.")
-        time.sleep(0.5)
-        clear()
-        menu()
-
-
-def wiiu():
-    global total
-    time.sleep(0.2)
-    os.remove("Codes.txt")
-    file = open("Codes.txt","w")
-    clear()
-    print("3ds and Wii-U Friend Code Generator")
-    print("")
-    print("How many codes to generate: ")
-    amt = int(input())
-    print("Generating...")
-    total = 0
-    total1 = 0
-    file = open("codes.txt","w") 
-    while total < amt:
-        pc = 200/(int(amt)+(int(total)))
-        total1 + 1
-        fc = str(randint(1000, 9999)) + "-" + str(randint(1000, 9999)) + "-" + str(randint(1000,9999))
-        total = total+1
-        if total % 1000 == 0:
-            title(str(name) + " - " + str(round(total*pc)) + "% completed")
-        file.write(str(fc)+"\n")
-    clear()
-    file.close()
-    if total == 1:
-        print("Completed! Generated "+ str(total) +" code!")
-    else:
-        print("Completed! Generated "+ str(total) +" codes!")
-    title("Friend Code Generator - 2.0.4")
-    input("Press enter to close! Thanks for using!")
-    close()
-
-def switch():
-    time.sleep(0.2)
-    os.remove("Codes.txt")
-    file = open("Codes.txt","w")
-    clear()
-    print("Switch Friend Code Generator")
-    print("")
-    print("How many codes to generate: ")
-    amt = int(input())
-    print("Generating...")
-    total = 0
-    file = open("Codes.txt","w") 
-    while total < amt:
-        pc = 200/(int(amt)+(int(total)))
-        fc = str(randint(1000, 9999)) + "-" + str(randint(1000, 9999)) + "-" + str(randint(1000,9999))
-        total = total+1
-        if total % 1000 == 0:
-            title(str(name) + " - " + str(round(total*pc)) + "% completed")
-        file.write("SW-" + str(fc)+"\n")
-    clear()
-    file.close()
-    finish()
-
-
-def steam():
-    time.sleep(0.2)
-    os.remove("Codes.txt")
-    file = open("Codes.txt","w")
-    clear()
-    print("Steam Friend Code Generator")
-    print("")
-    print("How many codes should be generated? ")
-    amt = int(input())
-    print("Generating...")
-    total = 0
-    file = open("Codes.txt","w")
-    while total < amt:
-        pc = 200/(int(amt)+(int(total)))
-        fc = str(randint(0,999999999))
-        total = total+1
-        if total % 100 == 0:
-            title(str(name) + " - " + str(round(total*pc)) + "% completed")
-        file.write(str(fc)+"\n")
-    clear()
-    file.close()
-    finish()
-
-def finish():
-    print("Completed!")
-    title("[" + str(ver) + "] " + str(name))
-    input("Press enter to close.")
-    exit()
-
+    print(f"Generating - {amount} codes.")
+    print("Type 1, 2 or 3 then press ENTER")
+    while True:
+        x = input("")
+        if x.isnumeric():
+            x = int(x)
+            match x:
+                case 1:
+                    clear()
+                    generator(amount, 0)
+                case 2:
+                    clear()
+                    generator(amount, 1)
+                case 3:
+                    clear()
+                    generator(amount, 2)
+                case _:
+                    print("Put a number between 1-3")
+                    continue
+        else:
+            print("Put a number")
+            continue
 menu()
